7ea41bb0616732dae058bd896d286365aff3dfbe
kurohost <smart_h0stag3@protonmail.com>
2021-01-31 22:42:56 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$('html, body').animate({
+    scrollTop: $("#top-image").offset().top
+  }, 700);
+
+
+function moreInfoAnimation() {
+    $('html, body').animate({
+        scrollTop: $("#more-infos").offset().top
+      }, 700);
+}
